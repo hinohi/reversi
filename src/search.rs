ee354aa944923ced2f33eaab8873cbddcb7ad7b0
Nakayama Daiju <42.daiju@gmail.com>
2021-01-10 13:02:13 +0900

    CountTurn スコアの Ord を改善

--- src/search.rs
+++ src/search.rs
@@ -8,70 +8,62 @@ pub trait Score: Copy + PartialOrd {
     fn flip(&self) -> Self;
 }
 
+/// Count & Turn
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
-pub struct CountWithTurn {
+pub struct CountTurn {
     pub mine: Count,
     pub opp: Count,
     pub turn: Turn,
 }
 
-impl CountWithTurn {
-    pub const fn new(mine: Count, opp: Count, turn: Turn) -> CountWithTurn {
-        CountWithTurn { mine, opp, turn }
+impl CountTurn {
+    pub const fn new(mine: Count, opp: Count, turn: Turn) -> CountTurn {
+        CountTurn { mine, opp, turn }
     }
 
-    pub const fn with_side(side: Side, black: Count, white: Count, turn: Turn) -> CountWithTurn {
+    pub const fn with_side(side: Side, black: Count, white: Count, turn: Turn) -> CountTurn {
         match side {
-            Side::Black => CountWithTurn::new(black, white, turn),
-            Side::White => CountWithTurn::new(white, black, turn),
+            Side::Black => CountTurn::new(black, white, turn),
+            Side::White => CountTurn::new(white, black, turn),
         }
     }
 }
 
-impl PartialOrd for CountWithTurn {
-    fn partial_cmp(&self, other: &CountWithTurn) -> Option<std::cmp::Ordering> {
-        use std::cmp::{Ordering::*, Reverse};
-        if self.opp == 0 {
-            if other.opp == 0 {
-                // 両者とも全滅させてるなら
-                // 1. 早い方が良い
-                // 2. 自分が多い方が良い (これは turn が同じなら同じなのが普通)
-                (Reverse(self.turn), self.mine).partial_cmp(&(Reverse(other.turn), other.mine))
-            } else {
-                // 全滅の方が良い
-                Some(Greater)
-            }
-        } else {
-            if other.opp == 0 {
-                // 全滅の方が良い
-                Some(Less)
-            } else {
-                // 割合ではなく絶対値
-                let a = (self.mine, Reverse(self.opp), Reverse(self.turn));
-                let b = (other.mine, Reverse(other.opp), Reverse(other.turn));
-                a.partial_cmp(&b)
-            }
+impl Ord for CountTurn {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        use std::cmp::{Ordering::*, Reverse as R};
+        match (self.mine, self.opp, other.mine, other.opp) {
+            // 両者とも全滅させてるなら
+            // 1. 早い方が良い
+            // 2. 自分が多い方が良い (これは turn が同じなら同じなのが普通)
+            (sm, 0, om, 0) => (R(self.turn), sm).cmp(&(R(other.turn), om)),
+            // その逆
+            (0, so, 0, oo) => (self.turn, R(so)).cmp(&(self.turn, R(oo))),
+            // 全滅の方が良い
+            (_, 0, _, _) => Greater,
+            (_, _, _, 0) => Less,
+            // 割合ではなく絶対値で比較
+            (sm, so, om, oo) => (sm, R(so), R(self.turn)).cmp(&(om, R(oo), R(other.turn))),
         }
     }
 }
 
-impl Score for CountWithTurn {
-    const MIN: CountWithTurn = CountWithTurn::new(0, (SIZE * SIZE) as Count, 0);
-    const MAX: CountWithTurn = CountWithTurn::new((SIZE * SIZE) as Count, 0, 0);
-    fn flip(&self) -> CountWithTurn {
-        CountWithTurn::new(self.opp, self.mine, self.turn)
+impl PartialOrd for CountTurn {
+    fn partial_cmp(&self, other: &CountTurn) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
 }
 
-pub fn search_exact<B: Board>(board: &B, side: Side) -> CountWithTurn {
-    exact_inner(
-        board,
-        side,
-        false,
-        0,
-        CountWithTurn::MIN,
-        CountWithTurn::MAX,
-    )
+impl Score for CountTurn {
+    const MIN: CountTurn = CountTurn::new(0, (SIZE * SIZE) as Count, 0);
+    const MAX: CountTurn = CountTurn::new((SIZE * SIZE) as Count, 0, 0);
+    fn flip(&self) -> CountTurn {
+        CountTurn::new(self.opp, self.mine, self.turn)
+    }
+}
+
+pub fn search_exact<B: Board>(board: &B, side: Side) -> CountTurn {
+    exact_inner(board, side, false, 0, CountTurn::MIN, CountTurn::MAX)
 }
 
 fn exact_inner<B: Board>(
@@ -79,14 +71,14 @@ fn exact_inner<B: Board>(
     side: Side,
     passed: bool,
     turn: u8,
-    mut alpha: CountWithTurn,
-    beta: CountWithTurn,
-) -> CountWithTurn {
+    mut alpha: CountTurn,
+    beta: CountTurn,
+) -> CountTurn {
     let candidates = board.list_candidates(side);
     if candidates.is_empty() {
         return if passed {
             let (black, white) = board.count();
-            CountWithTurn::with_side(side, black, white, turn)
+            CountTurn::with_side(side, black, white, turn)
         } else {
             exact_inner(board, side.flip(), true, turn, beta.flip(), alpha.flip()).flip()
         };
@@ -118,11 +110,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn count_with_turn_ord() {
-        assert!(CountWithTurn::new(40, 24, 10) > CountWithTurn::new(39, 24, 10));
-        assert!(CountWithTurn::new(40, 24, 10) < CountWithTurn::new(40, 23, 10));
-        assert!(CountWithTurn::new(60, 4, 3) < CountWithTurn::new(10, 0, 3));
-        assert!(CountWithTurn::new(10, 0, 10) > CountWithTurn::new(11, 0, 11));
-        assert!(CountWithTurn::new(10, 0, 10) < CountWithTurn::new(11, 0, 10));
+    fn count_with_turn_less_or_greater() {
+        // a < b
+        let cases = &[
+            (CountTurn::new(39, 24, 10), CountTurn::new(40, 24, 10)),
+            (CountTurn::new(40, 24, 10), CountTurn::new(40, 23, 10)),
+            (CountTurn::new(60, 4, 3), CountTurn::new(10, 0, 3)),
+            (CountTurn::new(11, 0, 11), CountTurn::new(10, 0, 10)),
+            (CountTurn::new(10, 0, 10), CountTurn::new(11, 0, 10)),
+        ];
+        for (a, b) in cases {
+            assert!(a < b);
+            assert!(b > a);
+        }
     }
 }
