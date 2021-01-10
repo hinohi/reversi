10239c55d636cd63ff57ca7b954f53542836eee5
Nakayama Daiju <42.daiju@gmail.com>
2021-01-11 00:56:50 +0900

    loop

--- src/bin/stats_random.rs
+++ src/bin/stats_random.rs
@@ -5,22 +5,24 @@ use reversi::{bit::BitBoard, search::RandomFullSearch, Game, Side};
 
 fn main() {
     let mut rng = Mcg128Xsl64::from_entropy();
-    for i in 0..=16 {
-        for j in 0..=16 {
-            let (b, w) = Game::new(
-                RandomFullSearch::<BitBoard, _>::new(
-                    Side::Black,
-                    Mcg128Xsl64::from_rng(&mut rng).unwrap(),
-                    64 - i,
-                ),
-                RandomFullSearch::<BitBoard, _>::new(
-                    Side::White,
-                    Mcg128Xsl64::from_rng(&mut rng).unwrap(),
-                    64 - j,
-                ),
-            )
-            .play_game();
-            println!("{} {} {} {}", i, j, b, w);
+    loop {
+        for i in 0..=16 {
+            for j in 0..=16 {
+                let (b, w) = Game::new(
+                    RandomFullSearch::<BitBoard, _>::new(
+                        Side::Black,
+                        Mcg128Xsl64::from_rng(&mut rng).unwrap(),
+                        64 - i,
+                    ),
+                    RandomFullSearch::<BitBoard, _>::new(
+                        Side::White,
+                        Mcg128Xsl64::from_rng(&mut rng).unwrap(),
+                        64 - j,
+                    ),
+                )
+                .play_game();
+                println!("{} {} {} {}", i, j, b, w);
+            }
         }
     }
 }
